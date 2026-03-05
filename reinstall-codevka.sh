8328eabee8beeaf187f04a0ad0dc2a74d249080a
Roman Frołow <rofrol@gmail.com>
2026-03-05 01:03:37 +0100

    Codevka: do not copy non-Nerd Fonts variants to repo

--- reinstall-codevka.sh
+++ reinstall-codevka.sh
@@ -5,8 +5,8 @@ echo ${DIR}
 ln -sf ${DIR}/private-build-plans-Codevka.toml ${DIR}/vendor/Iosevka/private-build-plans.toml &&
   cd ${DIR}/vendor/Iosevka &&
   npm run build -- ttf-unhinted::Codevka &&
-  cp ${DIR}/vendor/Iosevka/dist/Codevka/TTF-Unhinted/Codevka-ExtendedSemiLight.ttf ${DIR}/ &&
-  cp ${DIR}/vendor/Iosevka/dist/Codevka/TTF-Unhinted/Codevka-Extended.ttf ${DIR}/ &&
+  # cp ${DIR}/vendor/Iosevka/dist/Codevka/TTF-Unhinted/Codevka-ExtendedSemiLight.ttf ${DIR}/ &&
+  # cp ${DIR}/vendor/Iosevka/dist/Codevka/TTF-Unhinted/Codevka-Extended.ttf ${DIR}/ &&
   cd ${DIR}/vendor/FontPatcher &&
   ## seems like this makegroups does not work for `kitty +list-fonts --psnames`
   ## --makegroups needed to have Postscript name https://github.com/ryanoasis/nerd-fonts/issues/579#issuecomment-1441612101
